e6ecb182fb0678c8895978c31e1e5165ee8b2947
Manuel Galli <gmanuel89+github@gmail.com>
2017-02-28 16:05:41 +0100

    Update iMatrixSpray Method Gcode Generator.py

--- iMatrixSpray Method Gcode Generator.py
+++ iMatrixSpray Method Gcode Generator.py
@@ -7,7 +7,7 @@ program_version = "2017.02.28.1"
 ### GitHub URL where the R file is
 github_iMatrix_url = "https://raw.githubusercontent.com/gmanuel89/iMatrixSpray/master/iMatrixSpray%20Method%20Gcode%20Generator.py"
 ### Name of the file when downloaded
-script_file_name = "iMatrixSpray gcode method generator.py"
+script_file_name = "iMatrixSpray Method Gcode Generator.py"
 
 
 
